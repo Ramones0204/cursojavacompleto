3e95dea3cca9c13c7c437bc015a54c848fa03ca0
Ramones0204 <ramonroliveirafilho@gmail.com>
2019-12-19 16:34:37 -0300

    Classe Departamento

---
+++ DemoJDBC/src/aplicacao/Programa.java
@@ -0,0 +1,15 @@
+package aplicacao;
+
+import entidades.Departamento;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Departamento obj = new Departamento(1,"Livros");
+		System.out.println(obj);
+		
+		
+	}
+
+}
---
+++ DemoJDBC/src/db/DBIntegridadeException.java
@@ -0,0 +1,10 @@
+package db;
+
+public class DBIntegridadeException extends RuntimeException {
+	private static final long serialVersionUID = 1;
+	
+	public DBIntegridadeException(String msg) {
+		super();
+	}
+	 
+}
